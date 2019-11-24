45d91b84c5cc54f32b36d8e411c8ed430e2213f1
99__S2 <elephant890@naver.com>
2019-11-24 23:42:00 +0900

    fixed back button action

--- app/src/main/java/com/example/washerproject/SubActivity.java
+++ app/src/main/java/com/example/washerproject/SubActivity.java
@@ -1,11 +1,19 @@
 package com.example.washerproject;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SubActivity extends AppCompatActivity {
+    // 마지막으로 뒤로가기 버튼을 눌렀던 시간 저장
+    private long backKeyPressedTime = 0;
+    // 첫 번째 뒤로가기 버튼을 누를때 표시
+    private Toast toast;
 
     @Override
     protected void onCreate(Bundle bundle){
@@ -13,10 +21,39 @@ public class SubActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sub);
     }
 
-    //버튼을 눌렀을때 데이터를 담아서 팝업 액티비티를 호출하는 메소드
+    //버튼을 눌렀을때 팝업 액티비티를 호출하는 메소드
     public void mOnPopupClick(View v){
         Intent intent = new Intent(this, PopupActivity.class);
-        //intent.putExtra("data", "Test Popup");
         startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        // 기존 뒤로가기 버튼의 기능을 막기위해 주석처리 또는 삭제
+        // super.onBackPressed();
+
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간에 2초를 더해 현재시간과 비교 후
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간이 2초가 지났으면 Toast Show
+        // 2000 milliseconds = 2 seconds
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            toast = Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+            ViewGroup group = (ViewGroup) toast.getView();
+            TextView messageTextView = (TextView) group.getChildAt(0);
+            messageTextView.setTextSize(15);
+            toast.show();
+            return;
+        }
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간에 2초를 더해 현재시간과 비교 후
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간이 2초가 지나지 않았으면 종료
+        // 현재 표시된 Toast 취소
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            //finish();
+            ActivityCompat.finishAffinity(this);
+            System.runFinalization();
+            System.exit(0);
+            toast.cancel();
+        }
     }
 }
