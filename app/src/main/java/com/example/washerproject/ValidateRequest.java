2ed6a7d54b5e8eb53881171d96bd83155a9f65f0
juyoungl <pmis118@naver.com>
2019-11-30 21:50:29 +0900

    Changed status bar color and designed sign up layout

--- app/src/main/java/com/example/washerproject/ValidateRequest.java
+++ app/src/main/java/com/example/washerproject/ValidateRequest.java
@@ -5,6 +5,7 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
